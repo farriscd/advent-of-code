e069955bc8e6a40584551c69b8f6cca1d4879a48
Corey Farris <farris.cd@gmail.com>
2019-12-05 16:24:45 -0500

    Finished Day5, yay opcodes

--- day5/day5.cpp
+++ day5/day5.cpp
@@ -3,16 +3,30 @@
 #include <string>
 #include <vector>
 
-using namespace std;
+// This is hard written knowing that instruction codes are of a certain length
+std::vector<int> splitIntcode(int intcode)
+{
+    std::vector<int> splitIntcode;
+    // mode of 3rd parameter
+    splitIntcode.push_back((intcode / 10000) % 10);
+    // mode of 2nd parameter
+    splitIntcode.push_back((intcode / 1000) % 10);
+    // mode of 1st parameter
+    splitIntcode.push_back((intcode / 100) % 10);
+    // Opcode
+    splitIntcode.push_back(intcode % 100);
+
+    return splitIntcode;
+}
 
 int main()
 {
-    string line;
-    ifstream inputFile;
-    vector<int> v;
+    std::string line;
+    std::ifstream inputFile;
+    std::vector<int> v;
 
     // Read integer list into a vector
-    inputFile.open("test.txt");
+    inputFile.open("input.txt");
 
     if (inputFile.is_open())
     {
@@ -25,36 +39,100 @@ int main()
 
     for (int i = 0; i < v.size();)
     {
+        std::vector<int> intcode = splitIntcode(v[i]);
+        int param1, param2, param3;
+
+        param1 = intcode[2] == 1 ? v[i + 1] : v[v[i + 1]];
+        param2 = intcode[1] == 1 ? v[i + 2] : v[v[i + 2]];
+        // Currently dont really use param3
+        // param3 = intcode[0] == 1 ? v[i + 3] : v[v[i + 3]];
+
         // Addition - Opcode 1
-        if (v[i] == 1)
+        if (intcode[3] == 1)
         {
-            v[v[i + 3]] = v[v[i + 1]] + v[v[i + 2]];
+            v[v[i + 3]] = param1 + param2;
             i += 4;
         }
         // Multiplication - Opcode 2
-        else if (v[i] == 2)
+        else if (intcode[3] == 2)
         {
-            v[v[i + 3]] = v[v[i + 1]] * v[v[i + 2]];
+            v[v[i + 3]] = param1 * param2;
             i += 4;
         }
         // Input - Opcode 3
-        else if (v[i] == 3)
+        else if (intcode[3] == 3)
         {
-            int temp;
-            cout << "Input integer: ";
-            cin >> temp;
+            int temp = 0;
+            std::cout << "Input integer: ";
+            std::cin >> temp;
             v[v[i + 1]] = temp;
             i += 2;
         }
         // Output - Opcode 4
-        else if (v[i] == 4)
+        else if (intcode[3] == 4)
         {
-            cout << "Output integer: " << v[v[i + 1]] << '\n';
+            std::cout << "Output integer: " << param1 << '\n';
             i += 2;
         }
 
+        // Jump-if-true - Opcode 5
+        else if (intcode[3] == 5)
+        {
+            if (param1 != 0)
+            {
+                i = param2;
+            }
+            else
+            {
+                i += 3;
+            }
+        }
+
+        // Jump-if-false - Opcode 6
+        else if (intcode[3] == 6)
+        {
+            if (param1 == 0)
+            {
+                i = param2;
+            }
+            else
+            {
+                i += 3;
+            }
+        }
+
+        // Less than - Opcode 7
+        else if (intcode[3] == 7)
+        {
+            if (param1 < param2)
+            {
+                v[v[i + 3]] = 1;
+                i += 4;
+            }
+            else
+            {
+                v[v[i + 3]] = 0;
+                i += 4;
+            }
+        }
+
+        // Equals - Opcode 8
+        else if (intcode[3] == 8)
+        {
+            if (param1 == param2)
+            {
+                v[v[i + 3]] = 1;
+                i += 4;
+            }
+            else
+            {
+                v[v[i + 3]] = 0;
+                i += 4;
+            }
+        }
+
         // Exit - Opcode 99
-        else if (v[i] == 99)
+        else if (intcode[3] == 99)
         {
             //cout << "break" << '\n';
             break;
@@ -63,3 +141,6 @@ int main()
 
     return 0;
 }
+
+// Part One Correct Answer: 7265618
+// Part Two Correct Answer: 7731427
